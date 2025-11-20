698f2ba3d8a475186656d9a17377e7982e0b7f03
joelbobai <joelisaiahbobai@gmail.com>
2025-11-20 12:24:06 +0100

    first commit4

--- components/dashboard/Sidebar.tsx
+++ components/dashboard/Sidebar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+//import Image from "next/image";
 import { usePathname } from "next/navigation";
 
 import { useAuth } from "@/stores/auth-store";
@@ -19,7 +20,8 @@ export default function Sidebar() {
   return (
     <aside className="hidden h-screen w-64 flex-col border-r border-slate-100 bg-white px-6 py-8 shadow-sm lg:flex">
       <div className="space-y-1">
-        <p className="text-xs font-semibold uppercase tracking-wide text-slate-400">Manzo</p>
+        {/* <p className="text-xs font-semibold uppercase tracking-wide text-slate-400">Manzo</p> */}
+        <img src="https://www.manzo.ng/_next/image?url=%2FManzoNavLogo.png&w=256&q=75" alt="Manzo Logo"  />
         <p className="text-xl font-semibold text-slate-900">Admin Console</p>
         <p className="text-sm text-slate-500">{roleLabel}</p>
       </div>
